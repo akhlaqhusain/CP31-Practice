0701dc206fa72f159b33a7d321ef3a57ffe1b1ce
akhlaqhusain <akhlaqhusainoff@gmail.com>
2026-01-02 20:05:22 +0530

    Update

---
+++ 1200-Rated/D_Matryoshkas.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int,int>
+#define umii unordered_map<int,int>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define take(v) for(auto &x:v)cin>>x;
+#define prt(v) for(auto x:v)cout<<x<<' ';cout<<endl;
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+static constexpr auto MOD = 1000000007;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vi a(n);
+    take(a);
+    sort(all(a));
+    mii mp;
+    for(int i=0;i<n;i++){
+        if(mp[a[i]-1]==0){
+            mp[a[i]]++;
+        }else{
+            mp[a[i]-1]--;
+            mp[a[i]]++;
+        }
+    }
+    int ans = 0;
+    for(auto val:mp){
+        ans += val.second;
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+    unsigned t=1;
+    cin>>t;
+    while(t--)solve();
+}
+/*
+    author -> akhlaqhusain
+*/
+ 
